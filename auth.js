ff5c29cf2f14d5be86df8daf03bf131e2ccf20b2
Furatus <furatus@hotmail.fr>
2023-12-13 00:15:05 +0100

    Fixed a lot of bugs cant really tell which ones due to the amount, Added full jwt for auth, just need to apply it to all endpoints that needa connection now, and fix a lot of bugs as well but ah well gonna do that later

---
+++ auth.js
@@ -0,0 +1,49 @@
+import jwt  from "jsonwebtoken";
+import mongoose from "mongoose";
+import { user } from "./class/user.js";
+
+export default async function mwVerifytoken(req,res,next) {
+    const reqToken = req.headers.authorization;
+
+    if (reqToken === undefined) {
+        res.status(401);
+        return res.send ("401 - token not found. Please provide a token");
+    }
+
+    try {
+     const decoded = jwt.verify(reqToken,process.env.PASSPHRASE_TOKEN);
+     
+     try {
+        await mongoose.connect(process.env.MONGO_ADDRESS);
+        const userModel = mongoose.model("user", user.userSchema);
+        const founduser = await userModel.findOne({ pseudo: decoded }).exec();
+    
+            if (!founduser) {
+                return res.status(403).send("403 - Access Denied \nInvalid user.");
+            }
+    
+            req.user = founduser;
+        }
+        catch (error) {
+            return res.status(500).send("500 - Internal Server Error");
+        }
+
+    next();
+    } catch (error) {
+        return res.status(403).send("403 - Invalid Token. \n Please provide a valid token to have access to this ressource.");
+    }
+}
+
+export function mwAuthorizeRole(requiredRole) {
+    return function(req,res,next) {
+    const userRole = req.user.role;
+    
+    if (userRole !== requiredRole){
+        res.status(403).send("403 - Access Denied \nyou must have more permissions to access this ressource.");
+        req.user.isValid = false;
+    }
+    else {
+    next();
+    }
+    }
+}
