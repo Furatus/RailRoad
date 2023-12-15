e8dcbb22c954edfefbddb84dafa1607d553cf8a0
Furatus <furatus@hotmail.fr>
2023-12-15 14:36:43 +0100

    oops wrong file

--- routes/trainstation.route.js
+++ routes/trainstation.route.js
@@ -1,14 +1,11 @@
 import trainstation from "../class/trainstation.js";
 import {Router} from "express";
-import mwVerifytoken from "../auth.js";
-import { mwAuthorizeRole } from "../auth.js";
-import { role } from "../class/role.js";
 
 const trainstationRouter = Router();
 trainstationRouter.get("/id/:id", trainstation.callbackGetTrainStationbyId);
-trainstationRouter.post("/create", mwVerifytoken, trainstation.callbackCreateTrainStation);
+trainstationRouter.post("/create", trainstation.callbackCreateTrainStation);
 trainstationRouter.get("/name/:name", trainstation.callbackGetTrainStationbyName);
-trainstationRouter.delete("/delete", mwVerifytoken, /*mwAuthorizeRole["canDeleteTicket"],*/ trainstation.callbackDeleteTrainStationbyId);
+trainstationRouter.delete("/delete", trainstation.callbackDeleteTrainStationbyId);
 trainstationRouter.patch("/update", trainstation.callbackUpdateTrainStation);
 
 
